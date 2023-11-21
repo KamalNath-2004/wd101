cebb9688e5c8ca8e390b46f2850c294812185666
KamalNath-2004 <151562190+KamalNath-2004@users.noreply.github.com>
2023-11-21 19:27:01 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,51 @@
-
-  const form = document.getElementById('registrationForm');
-  const table = document.getElementById('formDataTable');
+const form = document.getElementById('registrationForm');
+  const tableBody = document.querySelector('#formDataTable tbody');
 
   form.addEventListener('submit', (event) => {
     event.preventDefault();
 
     const password = document.getElementById('password').value;
     const confirmPassword = document.getElementById('confirmPassword').value;
+    const email = document.getElementById('email').value;
+    const dob = new Date(document.getElementById('dob').value);
+    const termsChecked = document.getElementById('termsAndConditions').checked;
+
+    // Validate email format
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      document.getElementById('emailError').textContent = 'Invalid email format';
+      return;
+    } else {
+      document.getElementById('emailError').textContent = '';
+    }
+
+    // Validate age between 18 and 55
+    const today = new Date();
+    const age = today.getFullYear() - dob.getFullYear();
+    const monthDiff = today.getMonth() - dob.getMonth();
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < dob.getDate())) {
+      age--;
+    }
+
+    if (age < 18 || age > 55) {
+      document.getElementById('dobError').textContent = 'Age should be between 18 and 55';
+      return;
+    } else {
+      document.getElementById('dobError').textContent = '';
+    }
 
     if (password !== confirmPassword) {
       alert('Password and Confirm Password do not match');
       return;
     }
 
-    const formData = new FormData(form);
-    const formEntries = Array.from(formData.entries());
-
-    const userData = {};
-    formEntries.forEach(entry => {
-      const [key, value] = entry;
-      userData[key] = value;
-    });
+    const userData = {
+      name: document.getElementById('name').value,
+      email,
+      password,
+      dob: dob.toISOString().split('T')[0],
+      termsChecked: termsChecked ? 'Yes' : 'No'
+    };
 
     let savedData = JSON.parse(localStorage.getItem('userEntries')) || [];
     savedData.push(userData);
@@ -31,36 +56,20 @@
   });
 
   function displayTable() {
-    table.innerHTML = '';
+    tableBody.innerHTML = '';
 
     const savedData = JSON.parse(localStorage.getItem('userEntries')) || [];
-    if (savedData.length === 0) return;
-
-    const tableHeaders = Object.keys(savedData[0]);
-    const tableBody = document.createElement('tbody');
-
     savedData.forEach(data => {
       const bodyRow = document.createElement('tr');
 
-      tableHeaders.forEach(header => {
+      Object.values(data).forEach(value => {
         const td = document.createElement('td');
-        td.textContent = data[header];
+        td.textContent = value;
         bodyRow.appendChild(td);
       });
 
       tableBody.appendChild(bodyRow);
     });
-
-    const tableHead = table.createTHead();
-    const headRow = tableHead.insertRow();
-
-    tableHeaders.forEach(header => {
-      const th = document.createElement('th');
-      th.textContent = header;
-      headRow.appendChild(th);
-    });
-
-    table.appendChild(tableBody);
   }
 
   displayTable();
